52f9b540f5b42bbc874a57190a5f8dbb6f361574
Aryan Bastani <101284002+AryanBastani@users.noreply.github.com>
2024-04-29 20:19:49 +0330

    Generate unsynchrones actions

--- Test-Generating/123.py
+++ Test-Generating/123.py
@@ -12,7 +12,14 @@ def generateSyncheds(synchActions, numOfComponents):
     for i in range(numOfComponents):
         generateComponent(synchActions, synchOuts)
         
-
+def generateComponent(synchActions, synchOuts):
+    alphabets =  [i for i in all if i not in synchActions]
+    unsynchActs = list()
+    for i in range(numOfUnsynchActs):
+        newAct = alphabets[random.randint(0, len(alphabets) - 1)]
+        unsynchActs.append(newAct)
+        alphabets.remove(newAct)
+    
          
     
     
