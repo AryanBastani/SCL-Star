73533b5498937dd9e66aa096a92ca3b3c62d5a38
Aryan Bastani <101284002+AryanBastani@users.noreply.github.com>
2024-05-01 09:17:53 +0330

    Write in the dot file

--- Test-Generating/GenerateTest.py
+++ Test-Generating/GenerateTest.py
@@ -3,11 +3,13 @@ import random
 from typing import Final
 import pydot
 import GenerateComponent
+from itertools import product
+from string import ascii_lowercase
 
 
 class GenerateTest:
     def __init__(self):
-        self.alphabets = string.ascii_letters
+        self.alphabets = [''.join(i) for i in product(ascii_lowercase, repeat = 3)]
         self.numOfUnsynchActs: Final[int] = 2
         self.minStates: Final[int] = 5
         self.maxStates: Final[int] = 9
@@ -18,10 +20,13 @@ class GenerateTest:
         for synchAct in synchActions:
             synchOuts.append(random.randint(0,1))
         for i in range(numOfComponents):
+            self.componentCounter += 1
             unsynchActs = self.generateUnsynchActs(synchActions, synchOuts)
             numOfStates = random.randint(self.minStates, self.maxStates)
             graphString = GenerateComponent(synchActions, synchOuts, unsynchActs, numOfStates)
-        
+            with open('Component' + string(self.componentCounter) + '.dot', 'w') as dotFile:
+                dotFile.write(graphString)
+
         
     def generateUnsynchActs(self, synchActions, synchOuts):    
         unsynchActs = list()
