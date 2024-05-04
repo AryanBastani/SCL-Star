5b45fffeee96951f8a52690760219533ad4af2ec
Aryan Bastani <101284002+AryanBastani@users.noreply.github.com>
2024-05-04 13:03:40 +0330

    Implement the function generateActs

--- Test-Generating/GenerateTest.py
+++ Test-Generating/GenerateTest.py
@@ -6,11 +6,10 @@ import GenerateComponent as gc
 from itertools import product
 from string import ascii_lowercase
 
-
 class GenerateTest:
     def __init__(self):
         self.alphabets = [''.join(i) for i in product(ascii_lowercase, repeat = 3)]
-        self.numOfUnsynchActs: Final[int] = 2
+        self.numOfEachActs: Final[int] = 2
         self.minStates: Final[int] = 5
         self.maxStates: Final[int] = 9
         self.componentCounter = 0
@@ -23,7 +22,7 @@ class GenerateTest:
             
         for i in range(numOfComponents):
             self.componentCounter += 1
-            unsynchActs = self.generateUnsynchActs(synchActions, synchOuts)
+            unsynchActs = self.generateActs()
             numOfStates = random.randint(self.minStates, self.maxStates)
             
             componentGenerator = gc.ComponentGenerator(synchActions, synchOuts, unsynchActs, numOfStates)
@@ -33,15 +32,27 @@ class GenerateTest:
                 dotFile.close()
             print(graphString)
 
+    def generateAct(self):
+        newAct = self.alphabets[random.randint(0, len(self.alphabets) - 1)]
+        self.alphabets.remove(newAct)
+        return(newAct)
         
-    def generateUnsynchActs(self, synchActions, synchOuts):    
-        unsynchActs = list()
-        for i in range(self.numOfUnsynchActs):
-            newAct = self.alphabets[random.randint(0, len(self.alphabets) - 1)]
-            unsynchActs.append(newAct)
-            self.alphabets.remove(newAct)
+    def generateActs(self):    
+        acts = list()
+        for i in range(self.numOfEachActs):
+            acts.append(self.generateAct())
         
-        return(unsynchActs)
+        return(acts)
+    
+    def generatePointTPoint(self):
+        possibleNums = [4, 6, 8]
+        numOfComponents = random.randint(0, 2)
+        for twoComponents in range(0, possibleNums, 2):
+            synchActs = self.generateActs()
+            
+            
+            
+        
     
 gt = GenerateTest()
 gt.generateSynchComponents(['asd', 'ads'], 2)
